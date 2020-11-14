8027d5dae1be3d24d2deee37f6e18f56675d7756
ysgo <wjion8428@gmail.com>
2020-11-14 22:23:50 +0900

    2장: 스프링 부트에서 테스트 코드를 작성하기

---
+++ src/test/java/com/alone/boot/springboot/web/HelloControllerTest.java
@@ -0,0 +1,42 @@
+package com.alone.boot.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+  public void hello() throws Exception {
+    String hello = "hello";
+
+    mvc.perform(get("/hello"))
+        .andExpect(status().isOk())
+        .andExpect(content().string(hello));
+  }
+
+  @Test
+  public void helloDto() throws Exception {
+    String name = "hello";
+    int amount = 1000;
+
+    mvc.perform(get("/hello/dto")
+                  .param("name", name)
+                  .param("amount", String.valueOf(amount)))
+                  .andExpect(status().isOk())
+        .andExpect(status().isOk());
+//        .andExpect((ResultMatcher) jsonPath("$.name", is(name)))
+//        .andExpect((ResultMatcher) jsonPath("$.amount", is(amount)));
+  }
+}
