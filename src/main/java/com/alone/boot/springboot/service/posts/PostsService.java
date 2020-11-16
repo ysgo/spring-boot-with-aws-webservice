c54dcde1438b1bd2190933c9193e9778d769c0a7
ysgo <wjion8428@gmail.com>
2020-11-16 18:02:53 +0900

    4장: 머스테치로 화면 구성하기 - 전체 조회, 수정, 삭제 화면 만들기

--- src/main/java/com/alone/boot/springboot/service/posts/PostsService.java
+++ src/main/java/com/alone/boot/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.alone.boot.springboot.service.posts;
 
 import com.alone.boot.springboot.domain.posts.Posts;
 import com.alone.boot.springboot.domain.posts.PostsRepository;
+import com.alone.boot.springboot.web.dto.PostsListResponseDto;
 import com.alone.boot.springboot.web.dto.PostsResponseDto;
 import com.alone.boot.springboot.web.dto.PostsSaveRequestDto;
 import com.alone.boot.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +36,16 @@ public class PostsService {
     Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
     return new PostsResponseDto(posts);
+  }
+
+  @Transactional(readOnly = true)
+  public List<PostsListResponseDto> findAllDesc() {
+    return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+  }
+
+  @Transactional
+  public void delete(Long id) {
+    Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+    postsRepository.delete(posts);
   }
 }
--- src/main/java/com/alone/boot/springboot/web/IndexController.java
+++ src/main/java/com/alone/boot/springboot/web/IndexController.java
@@ -1,20 +1,33 @@
 package com.alone.boot.springboot.web;
 
+import com.alone.boot.springboot.service.posts.PostsService;
+import com.alone.boot.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+  private final PostsService postsService;
 
   @GetMapping("/")
-  public String index() {
+  public String index(Model model) {
+    model.addAttribute("posts", postsService.findAllDesc());
     return "index";
   }
 
   @GetMapping("/posts/save")
   public String postsSave() {
     return "posts-save";
+  }
+
+  @GetMapping("/posts/update/{id}")
+  public String postsUpdate(@PathVariable Long id, Model model) {
+    PostsResponseDto dto = postsService.findById(id);
+    model.addAttribute("post", dto);
+    return "posts-update";
   }
 }
