270d60838648d8342044127d370e43eecc27e065
ysgo <wjion8428@gmail.com>
2020-11-17 13:23:24 +0900

    5장: 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기 - 어노테이션 기반으로 개선하기

--- src/main/java/com/alone/boot/springboot/web/IndexController.java
+++ src/main/java/com/alone/boot/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.alone.boot.springboot.web;
 
+import com.alone.boot.springboot.config.auth.LoginUser;
 import com.alone.boot.springboot.config.auth.dto.SessionUser;
 import com.alone.boot.springboot.service.posts.PostsService;
 import com.alone.boot.springboot.web.dto.PostsResponseDto;
@@ -18,9 +19,8 @@ public class IndexController {
   private final HttpSession httpSession;
 
   @GetMapping("/")
-  public String index(Model model) {
+  public String index(Model model, @LoginUser SessionUser user) {
     model.addAttribute("posts", postsService.findAllDesc());
-    SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
     if(user != null) {
       model.addAttribute("userName", user.getName());
