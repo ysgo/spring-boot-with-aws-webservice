15af9bea0664dbcf3f9e22f54c3850e1affeefae
ysgo <wjion8428@gmail.com>
2020-11-18 21:41:11 +0900

    5장: 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기 - 네이버 로그인

--- src/main/java/com/alone/boot/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/alone/boot/springboot/config/auth/dto/OAuthAttributes.java
@@ -28,6 +28,9 @@ public class OAuthAttributes {
   // userNameAttributeName : OAuth2 로그인 진행 시 키가 되는 필드값. Primary key와 같은 의미
   // of 메서드는 OAuth2User 반환하는 사용자 정보가 Map이기 때문에 하나하나 변환이 필요함
   public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+    if("naver".equals(registrationId)) {
+      return ofNaver("id", attributes);
+    }
     return ofGoogle(userNameAttributeName, attributes);
   }
 
@@ -37,6 +40,17 @@ public class OAuthAttributes {
             .email((String) attributes.get("email"))
             .picture((String) attributes.get("picture"))
             .attributes(attributes)
+            .nameAttributeKey(userNameAttributeName)
+            .build();
+  }
+
+  private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+    Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+    return OAuthAttributes.builder()
+            .name((String) response.get("name"))
+            .email((String) response.get("email"))
+            .picture((String) response.get("profile_image"))
+            .attributes(response)
             .nameAttributeKey(userNameAttributeName)
             .build();
   }
